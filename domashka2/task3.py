4ad9ac9bc4e245bb70d02d6d93a620023f0e6340
Sanjar <44526842+SmiththeHacker@users.noreply.github.com>
2020-12-15 00:44:56 +0500

    Create task3.py

---
+++ domashka2/task3.py
@@ -0,0 +1,31 @@
+import os
+print('Чувак, я ещё не калькулятор, я только учусь, но определенные действия уже могу провернуть. Смотри сам')
+print()
+print('Введи два числа')
+print('Первое')
+x = int(input())
+print('Второе')
+y = int(input())
+print('А теперь введи то, что хочешь с ними сделать')
+os.system("clear")
+while True:
+    print()
+    print('Можно прибавить, для этого напиши "+", можно разделить, для этого напиши "/", могу и умножить для этого напиши "*" и, конечно же отнять, для этого просто "-"')
+    dey = input()
+    ans = 0
+    if dey == '+':
+        ans = x + y
+        os.system("clear")
+        print(ans)
+    if dey == '/':
+        ans = x / y
+        os.system("clear")
+        print(ans)
+    if dey == '*':
+        ans = x * y
+        os.system("clear")
+        print(ans)
+    if dey == '-':
+        ans = x - y
+        os.system("clear")
+        print(ans)
