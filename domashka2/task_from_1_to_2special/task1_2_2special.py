d5c7e79407f8efb4a230b5afb54ecc74e22ca9b4
Sanjar <44526842+SmiththeHacker@users.noreply.github.com>
2020-12-15 00:14:08 +0500

    Create task1_2_2special.py

---
+++ domashka2/task_from_1_to_2special/task1_2_2special.py
@@ -0,0 +1,27 @@
+print('Hi, I can count for you sum of numbers 1 to 100, look')
+y = 0
+x = 0
+while 100 > x:
+    x = x + 1
+    y = y + x
+    print('I`m counting, wait a minute...')
+print(y)
+
+print('Also I can count the sum of numbers from 1 to 14, including 14')
+y = 0
+x = 0
+while 14 > x:
+    x = x + 1
+    y = y + x
+    print('I`m counting, wait a minute...')
+print(y)
+
+print('You think that I can count only these? Hey, just write any number here')
+n = int(input())
+x = 0
+y = 0
+while n > x:
+    x = x + 1
+    y = y + x
+    print('I`m counting, wait a minute...')
+print(y)
