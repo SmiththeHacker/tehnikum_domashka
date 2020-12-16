152caa7b4fdaac2ebb7512ae8899830d0069f0c0
Sanjar <44526842+SmiththeHacker@users.noreply.github.com>
2020-12-17 02:40:58 +0500

    Create task1.py

---
+++ domashka3/task1.py
@@ -0,0 +1,15 @@
+print('Здравствуйте, я программа, которая умеет выводить каждый элемент вашего списка по отдельности')
+print()
+print('Давайте для начала добавим данные в список. Введите "stop", когда решите остановиться')
+base = []
+while True:
+    x = input()
+    base.append(x)
+    print(base)
+    print('Добавим что-то еще?')
+    if x == 'stop':
+        break
+for row in base:
+    for item in row:
+        print(item, end=' ')
+    print()
