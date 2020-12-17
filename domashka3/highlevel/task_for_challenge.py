6f245584dc1f1afeeb6eb32fe52470dc77952975
Sanjar <44526842+SmiththeHacker@users.noreply.github.com>
2020-12-17 18:29:37 +0500

    Update task_for_challenge.py

--- domashka3/highlevel/task_for_challenge.py
+++ domashka3/highlevel/task_for_challenge.py
@@ -1,7 +1,7 @@
+import random
 print('Давай сыграем в игру. Я загадаю число, а ты угадаешь. Если угадаешь, то получишь приз, а если нет, то ты умрешь')
 print('У тебя 3 попытки. Разумеется я тебе буду подсказывать')
 x = random.randint(1, 10)
-print(x)
 popitki = 3
 a = int(input('Что думаешь? Твой ответ: '))
 while True:
@@ -12,6 +12,57 @@ while True:
     if dif == 0:
         print('Феноменально! Как ты это сделал?')
         print()
+        print('Сыграем ещё?')
+        ans1 = input('Да или Нет: ')
+        if ans1 == 'Да':
+            print('Угадай число от 1 до 100')
+            f = random.randint(1, 100)
+            popitki = 10
+            while True:
+
+                if a > f:
+                    dif = a - f
+                else:
+                    dif = f - a
+                if dif == 0:
+                    print('Феноменально! Как ты это сделал?')
+                    print()
+                    print('Оцени нашу игру от 1 до 5')
+                    stars = int(input())
+                    if stars == 5:
+                        print('Огромное спасибо')
+                        break
+                    else:
+                        print('Мог бы поставить и больше, жмот')
+                        break
+                if dif == 1:
+                    print('Гориишь, горииишь')
+                    popitki = popitki - 1
+                    print('У тебя осталось', popitki, 'попытки/ок')
+                    a = int(input('Что скажешь теперь? '))
+                if 1 < dif < 20:
+                    print('Ты очень близок, мой друг')
+                    popitki = popitki - 1
+                    print('У тебя осталось', popitki, 'попытки/ок')
+                    a = int(input('Что скажешь теперь? '))
+                if 19 < dif < 50:
+                    print('Тепло')
+                    popitki = popitki - 1
+                    print('У тебя осталось', popitki, 'попытки/ок')
+                    a = int(input('Что скажешь теперь? '))
+                if 49 < dif < 80:
+                    print('Холодно')
+                    popitki = popitki - 1
+                    print('У тебя осталось', popitki, 'попытки/ок')
+                    a = int(input('Что скажешь теперь? '))
+                if dif >= 80:
+                    print('Очень  холодно')
+                    popitki = popitki - 1
+                    print('У тебя осталось', popitki, 'попытки/ок')
+                    a = int(input('Что скажешь теперь? '))
+                if popitki == 0:
+                    print('ПОТРАЧЕНО')
+                    break
         print('Оцени нашу игру от 1 до 5')
         stars = int(input())
         if stars == 5:
